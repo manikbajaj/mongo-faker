1b23d637dd7860b88c2ec29e62ec5ecf9a5baf68
Manik <manik@pixelconception.com>
2023-02-13 17:05:56 +0530

    Improved code coverage and removed unwanted directories from the coverage

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,23 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+  transform : {
+    '^.+\\.(t|j)s$' : 'ts-jest' ,
+  } ,
+  testRegex : '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)s$' ,
+  moduleFileExtensions : [ 'ts' , 'js' , 'json' , 'node' ] ,
+  rootDir : 'src' ,
+  collectCoverage : true ,
+  collectCoverageFrom : [
+    '**/*.{js,ts}' ,
+    '!**/node_modules/**' ,
+    '!**/vendor/**' ,
+    '!**/lib/**' ,
+    '!**/coverage/**'
+  ] ,
+  coverageDirectory : './coverage' ,
+  coverageThreshold : {
+    global : {
+      lines : 95,
+    },
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)s$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  collectCoverage: true,
-  collectCoverageFrom: ["./src/**"],
-  coverageDirectory: './coverage',
-  coverageThreshold: {
-    global: {
-      lines: 95
-    }
-  }
 };
